9d878a69f9f83d79020e8275bf0b4f752627fafb
Samyak Nagrale <samyaknagrale@Samyaks-MacBook-Air.local>
2026-03-24 20:02:30 +0530

    Solved 09 password strength question

--- src/09-password-strength.js
+++ src/09-password-strength.js
@@ -27,4 +27,57 @@
  */
 export function checkPasswordStrength(password) {
   // Your code here
+
+  if (typeof password !== "string") {
+    return "weak";
+  }
+
+  let pwdStrength = 0;
+
+  const specialCharSet = new Set("!@#$%^&*()_+-=[]{}|;:,.<>?");
+  // criteria 1
+  if (password.length >= 8) {
+    pwdStrength++;
+  }
+
+  let capFlag = false;
+  let smallFlag = false;
+  let numFlag = false;
+  let specialFlag = false;
+
+  for (let char of password) {
+    // criteria 2
+    if (char.charCodeAt() >= 65 && char.charCodeAt() <= 90 && !capFlag) {
+      pwdStrength++;
+      capFlag = true;
+    }
+
+    // criteria 3
+    if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122 && !smallFlag) {
+      pwdStrength++;
+      smallFlag = true;
+    }
+
+    // criteria 4
+    if (char.charCodeAt() >= 48 && char.charCodeAt() <= 57 && !numFlag) {
+      pwdStrength++;
+      numFlag = true;
+    }
+
+    //criteria 5
+    if (specialCharSet.has(char) && !specialFlag) {
+      pwdStrength++;
+      specialFlag = true;
+    }
+  }
+
+  if (pwdStrength <= 1) {
+    return "weak";
+  } else if (pwdStrength <= 3) {
+    return "medium";
+  } else if (pwdStrength == 4) {
+    return "strong";
+  } else {
+    return "very strong";
+  }
 }
